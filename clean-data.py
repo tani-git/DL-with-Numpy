5b84af9eca23b7855adcaf74b18114b1a888f39e
tani-git <31452671+tani-git@users.noreply.github.com>
2021-01-21 08:28:41 +0530

    Update clean-data.py

--- clean-data.py
+++ clean-data.py
@@ -1,9 +1,15 @@
 import os
 import shutil
 import cv2
+import pandas as pd
 
-def get_correct_data(src_path, des_path):
-	src_lis = [os.path.join(src_path, i) for i in os.listdir(src_path)]
+def get_correct_data(input_df, src_path, des_path):
+	df = pd.read_csv(input_df)
+	file = df.filename
+	src_lis = []
+	for i in file:
+		src_lis.append(os.path.join(src_path, i))
+	# src_lis = [os.path.join(src_path, i) for i in os.listdir(src_path)]
 	count = 0
 	for j in src_lis:
 		print("Total: {}, Remaining: {}".format(len(src_lis), len(src_lis) - count))
@@ -23,7 +29,8 @@ def get_correct_data(src_path, des_path):
 		count+=1
 				
 
+input_csv = "/home/code-ml/Desktop/SSD-TR/SSD-Mobilenet-Custom-Object-Detector-Model-using-Tensorflow-2/tensorflow_model/data/test_labels.csv"
 src_path = "/home/code-ml/Desktop/SSD-TR/TESE/zip-buckle/test"
 des_path = "/home/code-ml/Desktop/SSD-TR/TESE/zip-buckle/test_clean"
 
-get_correct_data(src_path, des_path)
+get_correct_data(input_csv,src_path, des_path)
